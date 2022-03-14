419f867c67d78b487a677dc015b4bdad14a9fb45
emsfregola <mswat@hotmail.it>
2022-03-14 15:20:22 +0100

    add menu toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log("Hi, nice to meet you! Welcome to my portfolio!");
+
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
